f9cf8dca35c8e6822dbda77581d821a45f552dcc
mliunardo <mliunardo@gmail.com>
2020-04-16 18:19:06 +0800

    Initial Commit

---
+++ src/main/java/com/rabbitmq/rabbitamqp/RabbitamqpApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbitamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitamqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitamqpApplication.class, args);
+	}
+
+}
